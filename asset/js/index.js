e89e1bb0da9a4ba910fcca2e5a024eb557889f23
HosseinFalah <98707258+HosseinFalah@users.noreply.github.com>
2022-05-22 00:01:52 +0430

    add button change category

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,4 +1,6 @@
-const searchData = document.querySelector("#search")
+const searchData = document.querySelector("#search");
+const allProducts = document.querySelector(".products__center");
+const btns = document.querySelectorAll(".btn")
 
 let allProductsData = []
 
@@ -14,19 +16,42 @@ document.addEventListener("DOMContentLoaded", () => {
             renderProducts(response.data, filters)
         })
         .catch(error => error)
-        console.log(allProductsData);
 })
 
 function renderProducts(_products, _filter){
     const filteredProducts = _products.filter(product => {
         return product.title.toLowerCase().includes(_filter.searchItems.toLowerCase())
     });
-    console.log(filteredProducts);
     //render to dom
+    allProducts.innerHTML = ''
+    filteredProducts.forEach((item, index) => {
+        // create
+        allProducts.insertAdjacentHTML("beforeend",
+        `<div class="product">
+            <div class="product__imgContainer">
+                <img src="${item.image}" class="product__img" alt="p-${index}">
+            </div>
+            <div class="product__desc">
+                <p class="product__title">${item.title}</p>
+                <p class="product__price">$${item.price}</p>
+            </div>
+        </div>`)
+        // content
+        // append to Product:
+    })
 }
 
 searchData.addEventListener("input", (e) => {
     filters.searchItems = e.target.value
     renderProducts(allProductsData, filters)
-    console.log(e.target.value);
+})
+
+//filter based on groups:
+
+btns.forEach(btn => {
+    btn.addEventListener("click", (event) => {
+        const filter = event.target.dataset.filter;
+        filters.searchItems = filter;
+        renderProducts(allProductsData, filters)
+    })
 })
